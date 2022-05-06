b9ab2871d4da291d9c40eec5e6d99888bb427dbc
Pablojonh6550 <Pablojonh6550@gmail.com>
2022-05-06 15:00:49 -0300

    version 1.4

--- src/components/Pokemon/Pokemon.jsx
+++ src/components/Pokemon/Pokemon.jsx
@@ -8,7 +8,7 @@ function Pokemon( props ) {
 
     const onHeartClick = () => {
         console.log("favorite");
-
+        
     }
 
 
@@ -16,33 +16,35 @@ function Pokemon( props ) {
         <div className="pokemon_container" >
             <div className="pokemon_card_hover">
                 <div className="pokemon_card">
-                    <div className="pokemon_image_container">
-                        <img className="pokemon_image" src={pokemon.sprites.front_default} alt={pokemon.name} />
-                    </div>
-
-                    <div className="pokemon_card_data">
-                        <div className="pokemon_name_container">
-                            <span>#{pokemon.id}</span>
-                            <h3>{pokemon.name}</h3>
+                    
+                        <div className={`pokemon_image_container ${!pokemon.types > 2 ? pokemon.types.reduce((type) => {return (type.type.name)}) : pokemon.types.map((type) => {return (type.type.name)})}`}>
+                            <img className="pokemon_image" src={pokemon.sprites.front_default} alt={pokemon.name} />
                         </div>
-
-                        <div className="type_container">
-                            <div className="type">
-                                {pokemon.types.map((type, index) => {                                
-                                    return (
-                                    <div key={index} className="pokemon_type_text">{type.type.name}</div>
-                                    
-                                    );
-                                    
-                                })}
+         
+                        <div className="pokemon_card_data">
+                            <div className="pokemon_name_container">
+                                <h3>{pokemon.name}</h3>
+                                <div className={`pokemon_number ${pokemon.types.map((type) => {return (type.type.name)})}`}>
+                                    <span>#{pokemon.id}</span> 
+                                </div>
                             </div>
-                        </div>
 
-                    </div>
+                            <div className="type_container">
+                                <div className="type">
+                                    {pokemon.types.map((type, index) => {                            
+                                        return (
+                                        <div key={index} className="pokemon_type_text">{type.type.name} {console.log(type.type.name)}</div>
+                                        
+                                        ); 
+                                    })}
+                                </div>
+                            </div>
+
+                        </div>
                         <div className="btn_favorite_container">
-                        <button className="btn_favorite" onClick={onHeartClick}>
-                            <AiFillHeart className="svg_btn"/>
-                        </button>
+                            <button className="btn_favorite" onClick={onHeartClick}>
+                                <AiFillHeart className="svg_btn"/>
+                            </button>
                         </div>
                 </div>
             </div>
